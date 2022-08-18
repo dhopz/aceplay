8b7fc38c169d4b23f7f65e5ec47489ab6e0db374
dhopz <david_houpapa@hotmail.com>
2022-08-18 10:16:55 +0100

    removing user and authorisation from test

--- src/test/java/tech/makers/aceplay/track/TracksControllerIntegrationTest.java
+++ src/test/java/tech/makers/aceplay/track/TracksControllerIntegrationTest.java
@@ -79,23 +79,8 @@ class TracksControllerIntegrationTest {
   @Test
   @WithMockUser
   void WhenLoggedIn_TracksPostCreatesNewTrack() throws Exception {
-    User kay = new User("kay", passwordEncoder.encode("pass"));
-    userRepository.save(kay);
-    MvcResult res =
-            mvc.perform(
-                            MockMvcRequestBuilders.post("/api/session")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    .content("{\"username\": \"kay\", \"password\": \"pass\"}"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.user.username").value("kay"))
-                    .andReturn();
-
-    String response = res.getResponse().getContentAsString();
-    String token = JsonPath.parse(response).read("$.token");
     mvc.perform(
                     MockMvcRequestBuilders.post("/api/tracks")
-                            .header("Authorization", token)
                             .contentType(MediaType.APPLICATION_JSON)
                             .content("{\"title\": \"Blue Line Swinger\", \"artist\": \"Yo La Tengo\", \"publicUrl\": \"https://example.org/track.mp3\"}"))
         .andExpect(status().isOk())
