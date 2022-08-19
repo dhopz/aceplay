d539caf3da656df79512cd0d88f65d1092b8bf50
dhopz <david_houpapa@hotmail.com>
2022-08-19 14:53:09 +0100

    exeption for empty now in services

--- src/main/java/tech/makers/aceplay/playlist/PlaylistService.java
+++ src/main/java/tech/makers/aceplay/playlist/PlaylistService.java
@@ -2,6 +2,7 @@ package tech.makers.aceplay.playlist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.makers.aceplay.EmptyFieldException;
 
 @Service
 public class PlaylistService {
@@ -9,7 +10,11 @@ public class PlaylistService {
     @Autowired PlaylistRepository playlistRepository;
 
     public Playlist addPlaylist(PlaylistRequestModel playlistRequestModel) {
-        Playlist playlist = new Playlist(playlistRequestModel.getName(), playlistRequestModel.getTracks(),playlistRequestModel.getUser());
-        return playlistRepository.save(playlist);
+        if(playlistRequestModel.getName() == null || playlistRequestModel.getName().isEmpty() || playlistRequestModel.getName().trim().isEmpty()) {
+            throw new EmptyFieldException("Empty Playlist Name");
+        } else {
+            Playlist playlist = new Playlist(playlistRequestModel.getName(), playlistRequestModel.getTracks(), playlistRequestModel.getUser());
+            return playlistRepository.save(playlist);
+        }
     }
 }
